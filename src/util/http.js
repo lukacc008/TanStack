0be582136700e839b31645eaee37d969f0ce10e0
unknown <lukacc008@gmail.com>
2024-05-17 16:07:31 +0200

    Query configuration object

--- src/util/http.js
+++ src/util/http.js
@@ -1,11 +1,12 @@
-export async function fetchEvents(searchTerm) {
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log('Search Term', searchTerm);
   let url = 'http://localhost:3000/events';
 
   if (searchTerm) {
     url += '?search=' + searchTerm
   }
 
-  const response = await fetch("http://localhost:3000/events");
+  const response = await fetch(url, {signal: signal});
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
