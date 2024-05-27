e678dc3937c597c2493df7b4a6963e2c5f00cb78
unknown <lukacc008@gmail.com>
2024-05-27 16:48:34 +0200

    Fetching data when view details button on certain event is clicked and displaying it correctly based on ID

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -37,7 +37,7 @@ export default function FindEventSection() {
 
   if (data) {
     content = (
-      <ul className="events-lost">
+      <ul className="events-list">
         {data.map((event) => (
           <li key={event.id}>
             <EventItem event={event} />
