33315d7bcb29ebe36ceeecc6fa1fb8bd1fefaf7b
unknown <lukacc008@gmail.com>
2024-06-03 16:53:06 +0200

    Updating data with mutations

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { fetchEvent, updateEvent } from "../../util/http.js";
 
 import Modal from "../UI/Modal.jsx";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
@@ -16,7 +16,14 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({id: params.id, event: formData});
+    navigate('../');
+  }
 
   function handleClose() {
     navigate("../");
